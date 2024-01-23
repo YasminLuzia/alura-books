89b8514f45500a51bea08d1cd340ad0df31d47ee
Yasmin <61717234+YasminLuzia@users.noreply.github.com>
2024-01-23 22:38:07 +0000

    First State

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,5 +1,6 @@
 import Input from "../Input"
 import styled from "styled-components"
+import { useState } from "react"
 
 const SearchContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -22,11 +23,12 @@ const Subtitle = styled.h3`
 `
 
 function Search() {
+    const [userText, setUserText] = useState('')
     return (
         <SearchContainer>
             <Title>Já sabe por onde começar?</Title>
             <Subtitle>Encontre seu livro em nossa estante.</Subtitle>
-            <Input placeholder="Escreva sua próxima leitura" />
+            <Input placeholder="Escreva sua próxima leitura" onBlur={event => setUserText(event.target.value)}/>
         </SearchContainer>
     )
 }
