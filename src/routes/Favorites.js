a045724d108b69bfcfdbcb022473065f003280a8
Yasmin <61717234+YasminLuzia@users.noreply.github.com>
2024-01-29 15:42:09 +0000

    Added styled components to favorite books

--- src/routes/Favorites.js
+++ src/routes/Favorites.js
@@ -1,15 +1,47 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { getFavorites } from '../services/favorites';
+import bookImage from '../img/book.png'
 
 const AppContainer = styled.div`
     width: 100vw;
     height: 100vh;
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
 `
+const ResultContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`
+const Result = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 0;
+    cursor: pointer;
+    text-align: center;
+    padding: 0 100px;
+    p {
+        width: 200px;
+        color: #FFF;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
+const Title = styled.h2`
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
+    padding-top: 35px
+`
 
 function Favorites() {
-  const [ favorites, setFavorites ] = useState([])
+  const [favorites, setFavorites] = useState([])
 
   async function fetchFavorites() {
     const favoritesAPI = await getFavorites()
@@ -18,15 +50,23 @@ function Favorites() {
 
   useEffect(() => {
     fetchFavorites()
-  })
+  }, [])
 
   return (
     <AppContainer>
-      {
-        favorites.map(favorite => (
-          <p>{favorite.name}</p>
-        ))
-      }
+      <div>
+        <Title>Aqui estão seus livros favoritos:</Title>
+        <ResultContainer>
+          {
+            favorites.length !== 0 ? favorites.map(favorite => (
+              <Result>
+                <p>{favorite.name}</p>
+                <img src={bookImage} alt='Capa do livro' />
+              </Result>
+            )) : null
+          }
+        </ResultContainer>
+      </div>
     </AppContainer>
   );
 }
